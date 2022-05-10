27248bb01715e2a900d3ffad4ffb7133c09b90d5
Jacob H <jacob.hannah164@gmail.com>
2022-05-09 20:03:42 -0400

    Created the JS file with simple test alert logs

---
+++ assets/js/game.js
@@ -0,0 +1,17 @@
+function fight () {
+    window.alert("The fight has begun!");
+}
+
+var playerName = window.prompt("What is your robots name?");
+
+console.log(playerName);
+
+console.log(10 + 10);
+
+console.log("Our robots name is " + playerName);
+
+var name = "Jacob";
+
+console.log(name);
+
+//fight();
