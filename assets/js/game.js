97018da1338ae4cdc855da491b8aa896579b06e0
Jacob H <jacob.hannah164@gmail.com>
2022-05-09 20:43:48 -0400

    initial game setup battle function complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,7 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
@@ -12,28 +13,55 @@ var enemyAttack = 12;
 var fight = function() {
     window.alert("Welcome to Robot Gladiators!");
 
-    //enemys turn
-    enemyHealth = enemyHealth - playerAttack;
-    console.log(playerName + " attacked " + enemyName + ". " +enemyName + " now has " + enemyHealth + " health.");
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
-    //displays the results of the players attacking turn
-    if (enemyHealth <= 0){
-        window.alert(enemyName + " has died!");
-    } 
-    else {
-        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+
+    //if user starts a new fight
+    if(promptFight === "fight" || promptFight === "FIGHT")
+    {
+        //enemys turn
+        enemyHealth = enemyHealth - playerAttack;
+        console.log(playerName + " attacked " + enemyName + ". " +enemyName + " now has " + enemyHealth + " health.");
+
+        //displays the results of the players attacking turn
+        if (enemyHealth <= 0){
+            window.alert(enemyName + " has died!");
+        } 
+        else {
+            window.alert(enemyName + " still has " + enemyHealth + " health left.");
+        }
+
+        //players turn
+        playerHealth = playerHealth - enemyAttack;
+        console.log(enemyName + " attacked " + playerName + ". " +playerName + " now has " + playerHealth + " health.");
+
+        //displays the results of the enemys attacking turn
+        if (playerHealth <= 0){
+            window.alert(enemyName + " has died!");
+        } 
+        else {
+            window.alert(playerName + " still has " + playerHealth + " health left.");
+        }
     }
 
-    //players turn
-    playerHealth = playerHealth - enemyAttack;
-    console.log(enemyName + " attacked " + playerName + ". " +playerName + " now has " + playerHealth + " health.");
+    //if the player skips the fight
+    else if(promptFight === "skip" || promptFight === "SKIP"){
+        window.alert(playerName + " has chosen to skip the fight!");
 
-    //displays the results of the enemys attacking turn
-    if (playerHealth <= 0){
-        window.alert(enemyName + " has died!");
-    } 
-    else {
-        window.alert(playerName + " still has " + playerHealth + " health left.");
+        var confirmSkip = window.confirm("Are you sure you would like to quit?");
+
+        if (confirmSkip){
+            window.alert(playerName + " has decided to skip this fight, Goodbye!");
+            playerMoney = playerMoney - 2;
+        }
+        else {
+            fight();
+        }
+    }
+
+    //if player selects no option displayed
+    else{
+        window.alert("Your need to choose a valid option. Try again!");
     }
 };
 
