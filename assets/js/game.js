9d8d0d8ec5473a00d81621b382fa1b8b78e8f9a6
Jacob H <jacob.hannah164@gmail.com>
2022-05-11 07:46:07 -0400

    Added the ability to fight multiple enemies

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,62 +8,69 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
-    window.alert("Welcome to Robot Gladiators!");
-
-    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+    //window.alert("Welcome to Robot Gladiators!"); remove
 
 
-    //if user starts a new fight
-    if(promptFight === "fight" || promptFight === "FIGHT")
-    {
-        //enemys turn
-        enemyHealth = enemyHealth - playerAttack;
-        console.log(playerName + " attacked " + enemyName + ". " +enemyName + " now has " + enemyHealth + " health.");
+    while(enemyHealth > 0){
+        var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
-        //displays the results of the players attacking turn
-        if (enemyHealth <= 0){
-            window.alert(enemyName + " has died!");
-        } 
-        else {
-            window.alert(enemyName + " still has " + enemyHealth + " health left.");
+
+        //if user starts a new fight
+        if(promptFight === "fight" || promptFight === "FIGHT")
+        {
+            //enemys turn
+            enemyHealth = enemyHealth - playerAttack;
+            console.log(playerName + " attacked " + enemyName + ". " +enemyName + " now has " + enemyHealth + " health.");
+
+            //displays the results of the players attacking turn
+
+
+                if (enemyHealth <= 0){
+                    window.alert(enemyName + " has died!");
+                } 
+                else {
+                    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+                }
+
+                //players turn
+                playerHealth = playerHealth - enemyAttack;
+                console.log(enemyName + " attacked " + playerName + ". " +playerName + " now has " + playerHealth + " health.");
+
+                //displays the results of the enemys attacking turn
+                if (playerHealth <= 0){
+                    window.alert(enemyName + " has died!");
+                } 
+                else {
+                    window.alert(playerName + " still has " + playerHealth + " health left.");
+                }
+            }
+
+        //if the player skips the fight
+        else if(promptFight === "skip" || promptFight === "SKIP"){
+            window.alert(playerName + " has chosen to skip the fight!");
+
+            var confirmSkip = window.confirm("Are you sure you would like to quit?");
+
+            if (confirmSkip){
+                window.alert(playerName + " has decided to skip this fight, Goodbye!");
+                playerMoney = playerMoney - 2;
+            }
+            else {
+                fight();
+            }
         }
 
-        //players turn
-        playerHealth = playerHealth - enemyAttack;
-        console.log(enemyName + " attacked " + playerName + ". " +playerName + " now has " + playerHealth + " health.");
-
-        //displays the results of the enemys attacking turn
-        if (playerHealth <= 0){
-            window.alert(enemyName + " has died!");
-        } 
-        else {
-            window.alert(playerName + " still has " + playerHealth + " health left.");
+        //if player selects no option displayed
+        else{
+            window.alert("Your need to choose a valid option. Try again!");
         }
-    }
-
-    //if the player skips the fight
-    else if(promptFight === "skip" || promptFight === "SKIP"){
-        window.alert(playerName + " has chosen to skip the fight!");
-
-        var confirmSkip = window.confirm("Are you sure you would like to quit?");
-
-        if (confirmSkip){
-            window.alert(playerName + " has decided to skip this fight, Goodbye!");
-            playerMoney = playerMoney - 2;
-        }
-        else {
-            fight();
-        }
-    }
-
-    //if player selects no option displayed
-    else{
-        window.alert("Your need to choose a valid option. Try again!");
     }
 };
 
 for(var i = 0; i < enemyNames.length; i++){
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 }
 
 //battle function of player vs robot
