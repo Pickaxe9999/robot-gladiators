35c35b26858b89354b900532838208e23f288b4c
Jacob H <jacob.hannah164@gmail.com>
2022-05-11 14:02:00 -0400

    added randomized turn orders to fights

--- assets/js/game.js
+++ assets/js/game.js
@@ -25,47 +25,53 @@ var fightOrSkip = function(){
 
 var fight = function(enemy) {
 
+    var isPlayersTurn = true;
+
+    if(Math.random() > 0.5){
+        isPlayersTurn = false;
+    }
+
     while(enemy.health > 0 && playerInfo.health > 0){
-        //Conditionall Recursive Function call
-        if(fightOrSkip()){
-            break;
+        if (isPlayersTurn){
+            //Conditionall Recursive Function call
+            if(fightOrSkip()){
+                break;
+            }
+            var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
+
+
+            enemy.health = Math.max(0, enemy.health - damage);
+            console.log(playerInfo.name + " attacked " + enemy.name + ". " +enemy.name + " now has " + enemy.health + " health.");
+
+            if (enemy.health <= 0){
+                window.alert(enemy.name + " has died!");
+                playerInfo.money = playerInfo.money + 20;
+                break;
+            } 
+            else {
+                window.alert(enemy.name + " still has " + enemy.health + " health left.");
+            }
+
+        }else{
+            //enemy attacks
+            var damage = randomNumber(enemy.attack - 3, enemy.attack);
+
+            playerInfo.health = Math.max(0, playerInfo.health - damage);
+            console.log(enemy.name + " attacked " + playerInfo.name + ". " +playerInfo.name + " now has " + playerInfo.health + " health.");
+            if (playerInfo.health <= 0){
+                window.alert(playerInfo.name + " has died!");
+                break;
+            } 
+            else {
+                window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
+            }
         }
 
-        //if user starts a new fight
-        //enemys turn
+        isPlayersTurn = !isPlayersTurn;
 
-        //generate random damage value based on player's attack power
-        var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
-
-        enemy.health = Math.max(0, enemy.health - damage);
-        console.log(playerInfo.name + " attacked " + enemy.name + ". " +enemy.name + " now has " + enemy.health + " health.");
-
-        //displays the results of the players attacking turn
-        if (enemy.health <= 0){
-            window.alert(enemy.name + " has died!");
-            break;
-        } 
-        else {
-            window.alert(enemy.name + " still has " + enemy.health + " health left.");
-        }
-
-        //players turn
-        //generate random damage value based on player's attack power
-        var damage = randomNumber(enemy.attack - 3, enemy.attack);
-
-        playerInfo.health = Math.max(0, playerInfo.health - damage);
-        console.log(enemy.name + " attacked " + playerInfo.name + ". " +playerInfo.name + " now has " + playerInfo.health + " health.");
-
-        //displays the results of the enemys attacking turn
-        if (playerInfo.health <= 0){
-            window.alert(enemy.name + " has died!");
-            break;
-        } 
-        else {
-            window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
-        }
     }
 };
+
 var startGame = function() {
     //reset the player stats
     playerInfo.reset();
